6b8e1cc5f72d432b5e6604c110f0440082ee5c70
snake_BDA0890519 <snake@cyber-dojo.org>
2016-02-09 00:59:26 +0000

    1

---
+++ sandbox/ProfitTest.java
@@ -0,0 +1,37 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ProfitTest {
+
+    @Test
+    public void should_calculate_total_profits_for_the_year_with_functions_returning_functions() {
+        // given
+        final double[] EXPECTED_SALES_JAN_TO_DEC =
+            new double[] { 42.0, 45.6, 43.6, 50.2, 55.6, 54.7,
+                    58.0, 57.3, 62.0, 60.3, 71.2, 88.8};
+        final FunctionOverTime sales =
+                FunctionOverTime.monthByMonth(EXPECTED_SALES_JAN_TO_DEC);
+
+        final FunctionOverTime fixedCosts =
+                FunctionOverTime.constant(15.0);
+
+        final FunctionOverTime incrementalCosts =
+                FunctionOverTime.line(5.1, 0.15);
+
+        final FunctionOverTime profit = FunctionOverTime.
+                combinationOf3(
+                        sales,
+                        fixedCosts,
+                        incrementalCosts,
+                        (s, fc, ic) -> s - ic - fc);;
+
+        // when
+        Double totalProfit = 0.0;
+        for(int time = 1; time <= 12; time ++) {
+            totalProfit += profit.valueAt(time);
+        }
+
+        // then
+        assertEquals(436.4, totalProfits, 0.001);
+    }
+}
